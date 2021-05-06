74d80b2dadfc96c3b482df8b4c618902a333e21b
Ilya_pinchuk <deffokorn71@gmail.com>
2021-05-06 22:47:09 +0300

    update 1.1

--- js/conditions.js
+++ js/conditions.js
@@ -1,16 +1,29 @@
 let age = +prompt("Your age?" )
 
-if (!parseInt(age)) {
+if ( age !== age) {
     alert('Нужно писать число!');
 }
 
-if (age < 2 ){
+else if(age % 10 === 0 ||  age % 10 >= 5 ||  age % 100 >= 5 && age % 100 <= 14   ){
+
+    alert(age + ' лет')
+}
+else if ( age % 10 === 1){
     alert(age + ' год')
 }
 
-else if(age < 5){
+else if ( age % 10 <= 4)
+{
     alert(age + ' года')
 }
-if(age > 4 || 0 ) {
-    alert(age + ' лет')
-}
+
+// if (age < 2 ){
+//     alert(age + ' год')
+// }
+//
+// else if(age < 5){
+//     alert(age + ' года')
+// }
+// if(age > 4 || 0 ) {
+//     alert(age + ' лет')
+// }
